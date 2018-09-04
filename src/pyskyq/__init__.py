59a6767c6481efd14c19906ed77cfdbbf2bfed60
Bradley Wood <git@bradleywood.com>
2018-09-04 10:43:49 +0100

    removed setuptools-scm and replaced with bumpversion

--- src/pyskyq/__init__.py
+++ src/pyskyq/__init__.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = 'unknown'
-
 from .skyq import SkyQ
 from .constants import rcmd, REMOTE_COMMANDS
+from ._version import __version__
