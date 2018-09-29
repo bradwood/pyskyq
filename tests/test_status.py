a155c2865bf35e5141224b255412b532f05f3695
Bradley Wood <git@bradleywood.com>
2018-09-29 20:58:15 +0100

    WIP: trying mock start() and stop() to no avail

--- tests/test_status.py
+++ tests/test_status.py
@@ -20,29 +20,31 @@ logging.basicConfig(level=logging.WARNING, stream=sys.stdout,
 def test_status(mocker):
     a = mocker.patch('websockets.connect', new_callable=AsyncContextManagerMock)
     a.return_value.__aenter__.return_value.recv = CoroutineMock(return_value=WS_STATUS_MOCK)
+    b = a.start()
+
 
     stat = Status('some_host')
-
     stat.create_event_listener()
+
     time.sleep(1) # allow time for awaiting, etc.
     assert stat.standby is True
 
-def wait_beyond_timeout_then_serve_json():
-    time.sleep(3)
-    raise websockets.exceptions.ConnectionClosed
-    #return WS_STATUS_MOCK
+# def wait_beyond_timeout_then_serve_json():
+#     time.sleep(3)
+#     raise websockets.exceptions.ConnectionClosed
+#     #return WS_STATUS_MOCK
 
 
 # def test_status_timeout(mocker):
 #     mocker.stopall()
 #     b = mocker.patch('websockets.connect', new_callable=AsyncContextManagerMock)
 #     b.return_value.__aenter__.return_value.recv = CoroutineMock(side_effect=wait_beyond_timeout_then_serve_json)
-
+#     b.start()
 #     stat = Status('timeout_host', ws_timeout=2)
-
 
 #     logging.getLogger().setLevel(logging.DEBUG)
 
 #     time.sleep(1)
 #     with pytest.raises(asyncio.TimeoutError):
 #         stat.create_event_listener()
+#     b.stop()
