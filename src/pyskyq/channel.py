b350572a389d5e8142690ea55f635720b3bdba7b
Bradley Wood <git@bradleywood.com>
2018-10-10 18:51:09 +0100

    improved channel docstrings

--- src/pyskyq/channel.py
+++ src/pyskyq/channel.py
@@ -13,32 +13,40 @@ LOGGER = logging.getLogger(__name__)
 class Channel:
     """This class holds channel data and methods for manipulating the channel.
 
-    Channel data can come form a variety of sources, including the SkyQ box itself (via)
-    a number of endppints, or from a remote XML TV feed.
+    Channel data can come form a variety of sources, including the SkyQ box itself
+    or from a remote XML TV feed.
 
-    The attributes are presented as read-only on the object.
-
-    For SkyQ-sourced Data
-    ---------------------
-    It will also dynamically
-    set attributes based on the payload obtained from the API, which itself could vary
-    from one channel to another. For example, channel's which are '+1' all carry the
-    ``timeshifted`` property whereas regular channels do not.
-
-
-    In order to present a more human-friendly API the
-    :const:`~pyskyq.constants.CHANNEL_FIELD_MAP` dictionary is used to provide access to
-    properties using more friendly names.
-
-    For XMLTV-sourced Data
-    ----------------------
-    TBC
+    This class has an extremely basic ``__init__()`` method, instead allowing data loading from
+    helper methods that are source-specific. It is probably better, however, to use the factory
+    functions to instantiate an object and load it with data.
 
     Note:
-        These channel attributes could change at any time with a box upgrade. The API will
-        attempt to adapt to ensure the new fields are presented.
+        Attributes are dynamically set based on the data payload provided, so if the SkyQ box
+        or XML TV feed is upgraded, new attributes *should* magically appear. Note that this
+        also implies that not every channel has every field, e.g., the ``timeshifted`` is only
+        present if it's ``True``. This is a Sky weirdness.
+
+    All attributes are read-only.
+
+    Note:
+        Data from XML TV sources presents the following channel attributes.
 
     Attributes:
+        xmltv_id (str): A hex string uniquely identifying this channel in the XML TV listing.
+            It is needed to look up programme's on the channel.
+        xmltv_icon_url (:py:class:`yarl.URL`): An internet URL to a graphic logo file.
+        xmltv_display_name (str): The human-friendly name of the channel. Should be the same
+            as ``name``.
+
+    Note:
+        The following attributes come from the SkyQ Box's various endpoints.
+
+    Attributes:
+        number (str): Human-friendly version of :attr:`c`.
+        quality (str): Human-friendly version of :attr:`sf`.
+        id (str): Human-friendly version of :attr:`sid`.
+        name (str):Human-friendly version of :attr:`t`.
+        desc (str): Human-friendly version of :attr:`upgradeMessage`.
         c (str): Channel number.
         dvbtriplet (str):  DVB Triplet (I have no idea what this is)
         schedule (bool): Is the channel "scheduled" or not?
@@ -52,17 +60,6 @@ class Channel:
         timeshifted (bool): Is this a ``+1``-style channel?
         upgradeMessage (str): A short description of the channel.
         isbroadcasting (boo): Is the channel broadcasting currently or not?
-
-    Note:
-        The below are the human-friendly mappings of some of Sky's terser-named
-        attributes.
-
-    Attributes:
-        number (str): Human-friendly version of :attr:`c`.
-        quality (str): Human-friendly version of :attr:`sf`.
-        id (str): Human-friendly version of :attr:`sid`.
-        name (str):Human-friendly version of :attr:`t`.
-        desc (str): Human-friendly version of :attr:`upgradeMessage`.
 
     """
 
@@ -99,14 +96,29 @@ class Channel:
         f'xmltv_id={self.xmltv_id}, number={self.number}, name={self.name}>'
 
     @property
-    def sources(self):
-        """Return the sources flag."""
+    def sources(self) -> CSRC:
+        """Return the sources flag.
+
+        Returns:
+            CSRC: A flag of one or more :class:`~pyskyq.constants.CSRC` ORed together to
+                indicate which sources have been applied to this object.
+
+        """
         return self._sources
 
     def load_skyq_summary_data(self,
                                chan_dict: Dict[str, Any],
                                ) -> None:
-        """Load summary data from SkyQ box into the Channel"""
+        """Load summary data from SkyQ box into the Channel.
+
+        Args:
+            chan_dict (dict): A detail dict that is obtained from the
+                ``as/services/`` endpoint.
+
+        Returns:
+            None
+
+        """
         self._chan_dict.update(chan_dict)
         self._sources = self._sources | CSRC.skyq_service_summary # type: ignore
         LOGGER.debug(f"Channel SkyQ Summary Data Loaded  with {chan_dict}.")
@@ -116,7 +128,7 @@ class Channel:
         """Add additional properties obtained from the SkqQ detail endpoint to the object.
 
         Args:
-            detail_dict (dict): A detail dict that is passed in directly from the
+            detail_dict (dict): A detail dict that is obtained from the
                 ``as/services/details/<sid>`` endpoint.
 
         Returns:
@@ -130,7 +142,20 @@ class Channel:
                         xml_chan: Element,
                         base_url: URL = URL('http://www.xmltv.co.uk/'),
                         ) -> None:
-        """Take an XML TV Channel element and load it into the channel's data structure."""
+        """Take an XML TV Channel element and load it into the channel's data structure.
+
+        This method loads this object with data passed in from an XML TV channel element.
+
+        Args:
+            xml_chan (Element): A XML element of ``<channel>...</channel>`` tags.
+            base_url (URL): A :py:class:`~yarl.URL` prefix which is used contstruct the full
+                :attr:`~pyskyq.channel.Channel.xmltv_icon_url` property.
+
+        Returns:
+            None
+
+
+        """
         assert xml_chan.tag.lower() == 'channel'
         self._chan_dict['xmltv_id'] = xml_chan.attrib['id']
         for child in xml_chan:
@@ -158,7 +183,18 @@ def channel_from_skyq_service(skyq_chan: Dict[str, Any]) -> Channel:
     return chan
 
 def channel_from_xmltv_list(xml_chan: Element) -> Channel:
-    """Create a Channel object from an XMLTV channel element."""
+    """Create a Channel object from an XMLTV channel element.
+
+    This function is a Channel factory. It will generate a Channel object given a XML
+    channel element from an XML TV file.
+
+    Args:
+        xml_chan (Element): A XML element of ``<channel>...</channel>`` tags.
+
+    Returns:
+        Channel: A channel object with the XML TV data loaded.
+
+    """
 
     chan = Channel()
     chan.load_xmltv_data(xml_chan)
@@ -169,21 +205,29 @@ def merge_channels(chan_a: Channel,
                    ) -> Channel:
     """Merge two channels together and returns a new one with the data combined.
 
+    Args:
+        chan_a (Channel): First channel to merge.
+        chan_b (Channel): Second channel to merge.
+
+    Returns:
+        Channel: Merged channel, although see warning for gotchas.
+
     Warning:
         No checks are made to ensure these two channels represent the same real channel.
         You should check each first before merging, perhaps by matching on the ``name``
         or ``xmltv_display_name``.
 
-        If there are any key clashes ``chan_b``'s keys will overrite ``chan_a``'s with
-        the exception of the following:
-            - if the ``sid`` or ``id`` attribute is ``None`` on one and not the other, the other
-              will override regardless of the passed order.
-            - if the ``c`` or ``number`` attribute is ``None`` on one and not the other, the other
-              will override regardless of the passed order.
-            - if the ``t`` or ``name`` attribute is ``None`` on one and not the other, the other
-              will override regardless of the passed order.
-            - if the ``xmltv_id`` attribute is ``None`` one one and not the other, the other will
-              override, regardless of the passed order.
+    If there are any key clashes ``chan_b``'s keys will overrite ``chan_a``'s with
+    the exception of the following:
+
+    - if the ``sid`` or ``id`` attribute is ``None`` on one and not the other, the other
+        will override regardless of the passed order.
+    - if the ``c`` or ``number`` attribute is ``None`` on one and not the other, the other
+        will override regardless of the passed order.
+    - if the ``t`` or ``name`` attribute is ``None`` on one and not the other, the other
+        will override regardless of the passed order.
+    - if the ``xmltv_id`` attribute is ``None`` one one and not the other, the other will
+        override, regardless of the passed order.
 
     """
     new_chan = Channel()
